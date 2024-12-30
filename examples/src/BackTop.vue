ab8d68efe8a4674bc62e193a4ba379382facd1e7
shezhiwei <1197530962@qq.com>
2024-12-30 10:14:29 +0800

    再次添加代码

--- examples/src/BackTop.vue
+++ examples/src/BackTop.vue
@@ -2,4 +2,6 @@
   <div :style="{ height: '2000px' }">sadas</div>
   <n-back-top>Top</n-back-top>
   <n-back-top>Top</n-back-top>
+  <n-back-top>Top</n-back-top>
+  <n-back-top>Top</n-back-top>
 </template>
